211ab4d1347b2233ca2f884886b8bf7399a047cc
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2017-12-21 12:09:53 +0900

    Still working...

--- main_complex.cpp
+++
@@ -1,6 +0,0 @@
-#include "main.hpp"
-
-int main(int argc, char** argv) {
-  return run_simulation<InteractionExpansion<complex_number_solver> >(argc, argv);
-}
-
--- main_real.cpp
+++
@@ -1,5 +0,0 @@
-#include "main.hpp"
-
-int main(int argc, char** argv) {
-  return run_simulation<InteractionExpansion<real_number_solver> >(argc, argv);
-}
