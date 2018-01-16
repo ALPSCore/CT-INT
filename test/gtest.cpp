8d8667e90ad7944675d10c15936fe58d797cdb4c
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2018-01-16 16:36:24 +0900

    Added

---
+++ test/gtest.cpp
@@ -0,0 +1,304 @@
+#include <algorithm>
+#include "gtest.h"
+
+
+#include "gtest.hpp"
+
+TEST(LegendreMeasurement, Tnl)
+{
+    const int n_matsubara = 1;
+    const int n_legendre = 1000;
+    LegendreTransformer trans(n_matsubara, n_legendre);
+
+    //Check if norm is conserved
+    for (std::size_t im=0; im<n_matsubara; ++im) {
+        std::complex<double> tmp;
+        for (std::size_t il=0; il<n_legendre; ++il) {
+            tmp += trans.Tnl()(im,il)*std::conj(trans.Tnl()(im,il));
+        }
+        ASSERT_TRUE(std::abs(tmp-1.)<1E-5) << "norm is not conserved = " << tmp;
+    }
+
+    std::vector<double> legendre_values(n_legendre);
+    const double xval = 0.2;
+    trans.compute_legendre(xval, legendre_values);
+    ASSERT_TRUE(std::abs(legendre_values[1]-xval)<1E-8);
+    for (std::size_t il=1; il<n_legendre-1; ++il) {
+        double left_val = (il+1)*legendre_values[il+1];
+        double right_val = (2*il+1)*xval*legendre_values[il]-il*legendre_values[il-1];
+        ASSERT_TRUE(std::abs(left_val-right_val)<1E-8);
+    }
+}
+
+
+TEST(Boost, Binomial) {
+    const size_t k = 2;
+    for (size_t N=k; N<10; ++N) {
+        const double tmp = boost::math::binomial_coefficient<double>(N,k);
+        ASSERT_TRUE(std::abs(tmp-0.5*N*(N-1.0))<1E-8);
+    }
+}
+
+TEST(MyUtil, permutation) {
+    assert(permutation(3,1)==3);
+    assert(permutation(3,2)==6);
+}
+
+TEST(QuantumNumber, diagonal_GF) {
+    size_t n_site = 4;
+    size_t n_flavors = 2;
+    size_t N=1000;
+
+    const size_t n_rank=2;
+    const size_t n_af=2;
+    const size_t Nv=1;
+    const double eps=0;
+
+    green_function<double> gf(N, n_site, n_flavors);
+    for (int t=0; t<N; ++t) {
+        for (int flavor=0; flavor<n_flavors; ++flavor) {
+            for (int i=0; i<n_site; ++i) {
+                for (int j=0; j<n_site; ++j) {
+                    gf(t, i, j, flavor) = 0.0;
+                }
+                gf(t, i, i, flavor) = -0.5;
+            }
+        }
+    }
+
+    std::vector<vertex_definition<double> > vertices;
+    boost::multi_array<double,2> alpha(boost::extents[n_af][n_rank]);
+    std::fill(alpha.origin(), alpha.origin()+alpha.num_elements(), 0);
+
+    //for (size_t iv=0; iv<Nv; ++iv) {
+    std::vector<spin_t> flavors(n_rank);
+    std::vector<size_t> sites(2*n_rank);
+    flavors[0] = 0;
+    flavors[1] = 1;
+    sites[0] = 0;
+    sites[1] = 1;
+    sites[2] = 2;
+    sites[3] = 3;
+    vertices.push_back(vertex_definition<double>(2,2,flavors,sites,0.0,alpha,0));
+
+    int qs[] = {1, -1, 0, 0, 0, 0, 1, -1};
+    std::vector<std::vector<std::valarray<int> > > quantum_number_vertices;
+    std::vector<std::vector<std::vector<size_t> > > groups(n_flavors);
+    std::vector<std::vector<int> > group_map;
+    quantum_number_vertices = make_quantum_numbers<double,double,green_function<double> >(gf, vertices, groups, group_map, eps);
+    std::valarray<int> qs2(qs,n_site*n_flavors);
+    bool flag = true;
+    int i_af = 0;
+    for (int i=0; i<qs2.size(); ++i) {
+        if (qs2[i]!=quantum_number_vertices[0][i_af][i]) {
+            flag = false;
+        }
+    }
+    ASSERT_TRUE(flag);
+
+    //vertices[0].make_quantum_numbers(group_map, quantum_number_vertices[0].size()/n_flavors);
+
+    //ASSERT_TRUE(qs2==std::valarray<int>(quantum_number_vertices[0]));
+    //ASSERT_TRUE(1==1);
+    //ASSERT_TRUE(qs2==qs3);
+}
+
+TEST(UpdateStatistics, EstimateSpread) {
+    const double beta = 100;
+
+    std::vector<simple_vertex> vertices;
+    vertices.push_back(simple_vertex(0.0));
+    vertices.push_back(simple_vertex(0.3*beta));
+    ASSERT_TRUE(std::abs(compute_spread<std::vector<simple_vertex> >(vertices.begin(), vertices.end(), beta)/beta-0.3)<1E-5);
+
+    //vertices are distributed on [0,beta] uniformly.
+    for (int Nv=2; Nv<10; ++Nv) {
+        vertices.clear();
+        for (int iv=0; iv<Nv; ++iv) {
+            vertices.push_back(simple_vertex((beta*iv)/Nv));
+        }
+        ASSERT_TRUE(std::abs(compute_spread<std::vector<simple_vertex> >(vertices.begin(), vertices.end(), beta)/beta-(1-1./Nv))<1E-5);
+    }
+}
+
+TEST(TypeRange, Integer) {
+    ASSERT_TRUE(std::numeric_limits<int>::max()>=2147483647);
+}
+
+TEST(Util, Mymod) {
+    const double tau = 1.0, beta = 15.0;
+    for (int i=-10; i<=10; ++i) {
+        double x = tau+beta*i;
+        ASSERT_TRUE(std::abs(mymod(x,beta)-tau)<1E-10);
+    }
+}
+
+TEST(FastUpdate, BlockMatrixReplaceRowsColsSingular) {
+    typedef alps::numeric::matrix<double> matrix_t;
+
+    for (int i=0; i<20; ++i) {
+        double alpha = pow(0.1, 20*i);
+        double a=4., b=8.;
+
+        matrix_t G2(2,2), G2p(2,2), M2p(2,2);
+        std::vector<std::pair<int, int> > swap_list;
+
+        G2(0,0) = alpha;
+        G2(1,1) = alpha;
+        G2(1,0) = a;
+        G2(0,1) = a;
+
+        M2p(0,0) = alpha;
+        M2p(1,0) = -b;
+        M2p(0,1) = -b;
+        M2p(1,1) = alpha;
+        M2p.block() /= (alpha+b)*(alpha-b);
+
+        matrix_t Q(1,1), R(1,1), S(1,1);
+        Q(0,0) = b;
+        R(0,0) = b;
+        S(0,0) = alpha;
+
+        matrix_t M2p_fast = inverse(G2);
+        matrix_t Mmat, inv_tSp;
+        matrix_t tPp, tQp, tRp, tSp;
+
+        double det_rat_fast = compute_det_ratio_replace_rows_cols_safe(M2p_fast, Q, R, S, Mmat, inv_tSp);
+        double det_rat = (alpha-b)*(alpha+b)/((alpha-a)*(alpha+a));
+    }
+}
+
+TEST(FastUpdate, ReplaceDiagonalElements) {
+    typedef double T;
+    typedef alps::numeric::matrix<T> matrix_t;
+
+    const int N=10, m=2, offset=2;
+    //const int N=2, m=1, offset=0;
+    assert(m+offset<=N);
+
+    matrix_t A_old(N,N), A_new(N,N), new_elems(m,1);
+    std::vector<T> elems_diff(m);
+    std::vector<int> pos(m);
+
+    randomize_matrix(A_old, 100);
+    randomize_matrix(new_elems, 200);
+    A_new = A_old;
+    matrix_t invA_old = inverse(A_old);
+    for (int i=0; i<m; ++i) {
+        pos[i] = i+offset;
+    }
+    for (int i=0; i<m; ++i) {
+        elems_diff[i] = new_elems(i,0)-A_old(pos[i],pos[i]);
+        A_new(pos[i],pos[i]) = new_elems(i,0);
+    }
+
+    const T det_rat = determinant(A_new)/determinant(A_old);
+    const T det_rat_fast = compute_det_ratio_replace_diaognal_elements(invA_old, m, pos, elems_diff, true);
+    ASSERT_TRUE(std::abs((det_rat-det_rat_fast)/det_rat)<1E-8);
+
+    /* inverse matrix update */
+    matrix_t invA_new = inverse(A_new);
+    matrix_t invA_new_fast = invA_old;
+    compute_det_ratio_replace_diaognal_elements(invA_new_fast, m, pos, elems_diff, false);
+
+    ASSERT_TRUE(std::abs(alps::fastupdate::norm_square(invA_new-invA_new_fast))<1E-5);
+}
+
+TEST(SubmatrixUpdate, single_vertex_insertion_spin_flip)
+{
+    typedef std::complex<double> T;
+    const int n_sites = 3;
+    const double U = 2.0;
+    const double alpha = 1E-2;
+    const double beta = 200.0;
+    const int Nv_max = 2;
+    const int n_flavors = 2;
+    const int k_ins_max = 32;
+    const int n_update = 5;
+    const int seed = 100;
+
+    std::vector<double> E(n_sites);
+    boost::multi_array<T,2> phase(boost::extents[n_sites][n_sites]);
+
+    for (int i=0; i<n_sites; ++i) {
+        E[i] = (double) i;
+        //std::cout << phase[i] << std::endl;
+    }
+    for (int i=0; i<n_sites; ++i) {
+        for (int j=i; j<n_sites; ++j) {
+            phase[i][j] = std::exp(std::complex<double>(0.0, 1.*i*(2*j+1.0)));
+            phase[j][i] = myconj(phase[i][j]);
+        }
+    }
+
+    general_U_matrix<T> Uijkl(n_sites, U, alpha);
+
+    itime_vertex_container itime_vertices_init;
+    itime_vertices_init.push_back(itime_vertex(0, 0, 0.5*beta, 2, true));
+
+    /* initialize submatrix_update */
+    //SubmatrixUpdate<T> submatrix_update(k_ins_max, n_flavors, DiagonalG0<T>(beta), &Uijkl, beta, itime_vertices_init);
+    SubmatrixUpdate<T> submatrix_update(k_ins_max, n_flavors, OffDiagonalG0<T>(beta, n_sites, E, phase), &Uijkl, beta, itime_vertices_init);
+
+    submatrix_update.sanity_check();
+
+    /* init udpate_manager */
+    alps::params params;
+    params["BETA"] = beta;
+    params["FLAVORS"] = n_flavors;
+    params["N_MULTI_VERTEX_UPDATE"] = Nv_max;
+    params["DOUBLE_VERTEX_UPDATE_A"] = 1.0/beta;
+    params["DOUBLE_VERTEX_UPDATE_B"] = 1.0e-2;
+    VertexUpdateManager<T> manager(params, Uijkl, OffDiagonalG0<T>(beta, n_sites, E, phase), false);
+
+    /* initialize RND generator */
+    //std::vector<double> probs(Nv_max, 1.0);
+    boost::random::uniform_smallint<> dist(1,Nv_max);
+    boost::random::uniform_01<> dist01;
+    boost::random::mt19937 gen(seed);
+    boost::random::variate_generator<boost::random::mt19937&, boost::random::uniform_smallint<> > Nv_prob(gen, dist);
+    boost::random::variate_generator<boost::random::mt19937&, boost::random::uniform_01<> > random01(gen, dist01);
+
+    std::vector<alps::numeric::matrix<T> > M(n_flavors), M_scratch(n_flavors);
+
+
+    for (int i_update=0; i_update<n_update; ++i_update) {
+        T sign_from_M0, weight_from_M0;
+        boost::tie(sign_from_M0,weight_from_M0) = submatrix_update.compute_M_from_scratch(M_scratch);
+
+        //const T weight_rat = submatrix_update.vertex_insertion_removal_update(manager, random01);
+        const T weight_rat = manager.do_ins_rem_update(submatrix_update, Uijkl, random01, 1.0);
+        const T sign_bak = submatrix_update.sign();
+
+        ASSERT_TRUE(submatrix_update.sanity_check());
+        submatrix_update.recompute_matrix(true);
+        submatrix_update.compute_M(M);
+        T sign_from_M, weight_from_M;
+        boost::tie(sign_from_M,weight_from_M) = submatrix_update.compute_M_from_scratch(M_scratch);
+
+        ASSERT_TRUE(my_equal(weight_from_M/weight_from_M0, weight_rat, 1E-5));
+
+        //std::cout << "sign " << sign_bak << " " << submatrix_update.sign() << std::endl;
+        //std::cout << "sign_from_M " << sign_from_M << std::endl;
+        ASSERT_TRUE(std::abs(sign_bak-submatrix_update.sign())<1.0e-5);
+        ASSERT_TRUE(std::abs(sign_from_M-submatrix_update.sign())<1.0e-5);
+        //std::cout << " Nv " << submatrix_update.itime_vertices().num_interacting() << std::endl;
+        for (int flavor=0; flavor<n_flavors; ++flavor) {
+            if (M[flavor].size2()>0) {
+                ASSERT_TRUE(alps::fastupdate::norm_square(M[flavor]-M_scratch[flavor])/alps::fastupdate::norm_square(M[flavor])<1E-8);
+            }
+        }
+
+        const T weight_rat2 = manager.do_spin_flip_update(submatrix_update, Uijkl, random01);
+
+        T sign_from_M2, weight_from_M2;
+        boost::tie(sign_from_M2,weight_from_M2) = submatrix_update.compute_M_from_scratch(M_scratch);
+        ASSERT_TRUE(my_equal(weight_from_M2/weight_from_M, weight_rat2, 1E-5));
+
+        const T weight_rat3 = manager.do_shift_update(submatrix_update, Uijkl, random01, false);
+    }
+
+    //std::cout << DiagonalG0<T>(beta)(0.0) << std::endl;
+    //std::cout << DiagonalG0<T>(beta)(0.5*beta) << std::endl;
+    //std::cout << DiagonalG0<T>(beta)(0.9999*beta) << std::endl;
+}
---
+++ test/gtest.hpp
@@ -0,0 +1,118 @@
+#include <complex>
+#include <limits>
+
+#include <boost/random.hpp>
+#include <boost/math/special_functions/binomial.hpp>
+#include <boost/random.hpp>
+#include <boost/multi_array.hpp>
+#include <boost/range/irange.hpp>
+
+//#include <alps/numeric/matrix.hpp>
+//#include <alps/numeric/matrix/algorithms.hpp>
+#include "../src/matrix.hpp"
+
+#include "../src/legendre.h"
+#include "../src/util.h"
+#include "../src/fastupdate_formula.h"
+#include "../src/green_function.h"
+#include "../src/update_statistics.h"
+#include "../src/operator.hpp"
+#include "../src/submatrix.hpp"
+#include "../src/update_manager.hpp"
+
+template<class T>
+void randomize_matrix(alps::numeric::matrix<T>& mat, size_t seed=100) {
+    boost::random::mt19937 gen;
+    boost::random::uniform_01<double> dist;
+    gen.seed(seed);
+
+    //std::cout << " debug " << num_cols(mat) << " " << num_rows(mat) << std::endl;
+    for (int j=0; j<num_cols(mat); ++j) {
+        for (int i=0; i<num_rows(mat); ++i) {
+            mat(i,j) = dist(gen);
+        }
+    }
+}
+
+
+class simple_vertex {
+public:
+    simple_vertex(double time) : time_(time) {}
+    double time() const {return time_;}
+
+private:
+    double time_;
+};
+
+
+//Interpolation of G0
+template<typename T>
+struct DiagonalG0 {
+  DiagonalG0 (double beta) : beta_(beta) {}
+
+  int nsite() const {return 1;}
+  int nflavor() const {return 2;}
+  bool is_zero(int site1, int site2, int flavor, double eps) const {return site1!=site2;}
+
+  T operator() (double dt) const {
+    //const double dt = c_op.t().time()-cdagg_op.t().time();
+    if (dt>=0 && dt<beta_) {
+      return -(1.0 / (beta_ * beta_)) * (dt - 0.5 * beta_) * (dt - 0.5 * beta_) - 0.25;
+    } else if (dt>=beta_ && dt<2*beta_) {
+      double dt2 = dt-beta_;
+      return -(-(1.0 / (beta_ * beta_)) * (dt2 - 0.5 * beta_) * (dt2 - 0.5 * beta_) - 0.25);
+    } else if (dt>=-beta_ && dt<0) {
+      double dt2 = dt+beta_;
+    return -(-(1.0 / (beta_ * beta_)) * (dt2 - 0.5 * beta_) * (dt2 - 0.5 * beta_) - 0.25);
+    }  else {
+      throw std::runtime_error("Digonaal G0: Not implemented!");
+    }
+  }
+
+  T operator() (const annihilator& c_op, const creator& cdagg_op) {
+    if (c_op.s()!=cdagg_op.s())
+      return 0.0;
+    if (c_op.flavor()!=cdagg_op.flavor())
+      return 0.0;
+
+    //std::cout << "--- " <<  " ---- " << std::endl;
+    double dt = c_op.t().time()-cdagg_op.t().time();
+    if (std::abs(dt)<1E-10) {
+      dt += c_op.t().small_index() > cdagg_op.t().small_index() ? 1E-8 : -1E-8;
+    }
+    return operator()(dt);
+  }
+
+  double beta_;
+};
+
+//Interpolation of G0
+template<typename T>
+struct OffDiagonalG0 {
+    OffDiagonalG0 (double beta, int n_site, const std::vector<double>& E, const boost::multi_array<T,2>& phase) : beta_(beta), n_site_(n_site), E_(E), phase_(phase) {}
+
+    int nsite() const {return n_site_;}
+    int nflavor() const {return 2;}
+    bool is_zero(int site1, int site2, int flavor, double eps) const {return false;}
+
+    T operator() (const annihilator& c_op, const creator& cdagg_op) {
+      const double dt = c_op.t().time()-cdagg_op.t().time();
+      double dt_tmp = dt;
+      if (dt_tmp > beta_) dt_tmp -= beta_;
+      if (dt_tmp < 0) dt_tmp += beta_;
+
+      if (c_op.s()==cdagg_op.s()) {
+        //return -(1.0 / (beta_ * beta_)) * (dt_tmp - 0.5 * beta_) * (dt_tmp - 0.5 * beta_) - 0.25 + (-dt+0.5*beta_)/(10*beta_);
+        const double E_tmp = E_[c_op.s()];
+        return  -std::exp((beta_-dt_tmp)*E_tmp)/(1.0+std::exp(beta_*E_tmp));
+      } else {
+        //std::cout << myconj(phase_[c_op.s()]) << " " << phase_[cdagg_op.s()] << myconj(phase_[c_op.s()])*phase_[cdagg_op.s()] << std::endl;
+        return (-dt+0.5*beta_)/(2*beta_)*phase_[c_op.s()][cdagg_op.s()];
+      }
+    }
+
+    double beta_;
+    int n_site_;
+    std::vector<double> E_;
+    boost::multi_array<T,2> phase_;
+};
